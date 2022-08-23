03bda6737c9c98ceaad1d3f58df6ebcb2f453a61
Marta Casal <marta.casal@tirabol.cat>
2022-08-23 17:26:56 +0200

    some postcss plugins added

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    "postcss-custom-properties": true,  /* Algo per les variables CSS */
+    "postcss-nesting": true,            /* Nesting CSS */
+    "autoprefixer": true,               /* CSS Vendor prefixes */
+  }
+}
